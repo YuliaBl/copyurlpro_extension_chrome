b331a7d3ec6fd270b8d26c43126efa14629801ef
polosatova <blazhenko.yulia@gmail.com>
2020-07-09 18:24:12 +0300

    working extention with async function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
-window.onload = function() {
-  chrome.tabs.query({ currentWindow: true, active: true },
-    function copyUrl(tabs) {
-    let urlCopy = document.getElementById('urltextarea');
-    console.log(urlCopy);
+// window.onload = function() {
+//   chrome.tabs.query({ currentWindow: true, active: true },
+//     function copyUrl(tabs) {
+//     let urlCopy = document.getElementById('urltextarea');
+//     console.log(urlCopy);
+//       urlCopy.innerHTML = tabs[0].url;
+//       console.log(urlCopy.innerHTML);
+//       urlCopy.select();
+//       document.execCommand('copy');
+//   });
+// };
+
+
+window.onload = function () {
+  chrome.tabs.query(
+    { currentWindow: true, active: true },
+    async function copyUrl(tabs) {
+      let urlCopy = document.getElementById('urltextarea');
+      console.log(urlCopy);
       urlCopy.innerHTML = tabs[0].url;
       console.log(urlCopy.innerHTML);
       urlCopy.select();
-      document.execCommand('copy');
-  });
+      try {
+        await navigator.clipboard.writeText(urlCopy.innerHTML);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  );
 };
