afac1ee2e4f8bc47826a187d1a18cef8e7b1653e
polosatova <blazhenko.yulia@gmail.com>
2020-07-09 19:30:54 +0300

    working extention ready to upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,3 @@
-// window.onload = function() {
-//   chrome.tabs.query({ currentWindow: true, active: true },
-//     function copyUrl(tabs) {
-//     let urlCopy = document.getElementById('urltextarea');
-//     console.log(urlCopy);
-//       urlCopy.innerHTML = tabs[0].url;
-//       console.log(urlCopy.innerHTML);
-//       urlCopy.select();
-//       document.execCommand('copy');
-//   });
-// };
-
-
 window.onload = function () {
   chrome.tabs.query(
     { currentWindow: true, active: true },
